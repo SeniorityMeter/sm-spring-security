db7cb728ff8d837f8d2fc4009cfbeeb18f769e0d
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2024-03-30 12:59:04 -0300

    feat: add token

--- src/main/java/com/senioritymeter/security/interaction/GenerateToken.java
+++ src/main/java/com/senioritymeter/security/interaction/GenerateToken.java
@@ -2,9 +2,8 @@ package com.senioritymeter.security.interaction;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import java.time.Instant;
-
 import com.senioritymeter.security.valueobject.Token;
+import java.time.Instant;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,16 +17,14 @@ public class GenerateToken {
   private String secret;
 
   public Token execute(final Input input) {
-    final var token = JWT.create()
-        .withIssuer(OSL_SECURITY_ISSUER)
-        .withSubject(input.getSubject())
-        .withExpiresAt(input.getExpiresAt())
-        .sign(Algorithm.HMAC512(secret));
+    final var token =
+        JWT.create()
+            .withIssuer(OSL_SECURITY_ISSUER)
+            .withSubject(input.getSubject())
+            .withExpiresAt(input.getExpiresAt())
+            .sign(Algorithm.HMAC512(secret));
 
-    return Token.builder()
-        .accessToken(token)
-        .expiresAt(input.getExpiresAt())
-        .build();
+    return Token.builder().accessToken(token).expiresAt(input.getExpiresAt()).build();
   }
 
   @Data
