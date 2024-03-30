db4a5a3ce426e88d52a871a500e1851ce0642bc5
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2024-03-30 12:52:31 -0300

    feat: add token

---
+++ src/main/java/com/senioritymeter/security/valueobject/Token.java
@@ -0,0 +1,12 @@
+package com.senioritymeter.security.valueobject;
+
+import java.time.Instant;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Token {
+  private String accessToken;
+  private Instant expiresAt;
+}
